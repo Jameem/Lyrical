3497eabc5c9780cafd708198c156a48400e84c62
Jameem <jameemkurikkal.mp@gmail.com>
2020-07-20 09:04:35 +0530

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: "./client/index.js",
+  output: {
+    path: "/",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        query: {
+          presets: ["react"],
+        },
+      },
+      {
+        use: ["style-loader", "css-loader"],
+        test: /\.css$/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "client/index.html",
+    }),
+  ],
+}
